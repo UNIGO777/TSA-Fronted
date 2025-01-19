07a2b9ae11d5d0f1d28382bb425ff742b424e50a
naman jain <naman13399@gmail.com>
2025-01-20 03:03:16 +0530

    first commit

---
+++ Admin_panel/Assests/Js/Logout.js
@@ -0,0 +1,9 @@
+
+const logoutbutton = document.getElementById('logoutbutton')
+
+
+const logout =  () => {
+    sessionStorage.removeItem('authToken'); // Remove expired token
+    sessionStorage.removeItem('tokenExpiry');
+    window.location.reload()
+}
